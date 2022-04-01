7652b7306382a788fb371a33aaeea26e528fac55
Thomas Pochart <tpochart@gmail.com>
2022-04-01 13:45:17 +0200

    fixed non-dsplayed funcs, add tot plot

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def rhs(n,t,a=1):
         R[k]=-a*s
     return(R)
 
-def plot_(list_n_ini, funcs=[], t_ini=0, t_fin=100, nb_t=10000, displayed_sites=[], a=1):
-    # We plot for multiple n_ini ; nb_sites is built-in in the len(n_ini[i]), which are assumed to all be the same
+def plot_(list_n_ini, funcs=[], funcs_names=[], t_ini=0, t_fin=100, nb_t=10000, displayed_sites=[], a=1):
+    # We plot selected n_k for multiple n_ini ; nb_sites is built-in in the len(n_ini[i]), which are assumed to all be the same
     # kwarg funcs : optionally, other functions to be plotted for comparison
+    # kwarg funcs_names: name of the other functions, to put in labels
     # kwargs t_ini, t_fin, t_nb : settings for t linspace
     # kwarg displayed_sites : choose which n_k to display (if a n_k is displayed, then it is displayed for all n_ini). [] means displaying all sites.
-    # kwarg a : a for rmb
+    # kwarg a : a for rhs
     if displayed_sites==[]:
         displayed_sites = list(range(len(list_n_ini[0])))
     t = np.linspace(t_ini,t_fin,nb_t)
@@ -29,6 +30,27 @@ def plot_(list_n_ini, funcs=[], t_ini=0, t_fin=100, nb_t=10000, displayed_sites=
         n = odeint(rhs, n_ini, t, args=(a,))
         for i in displayed_sites:
             plt.plot(t, np.array([x[i] for x in n]),label=f'$n_'+str(i)+'$'+r', $n_{ini}$='+str(n_ini))
+    for i in range(len(funcs)):
+            plt.plot(t, funcs[i](t),label=funcs_names[i])
     plt.legend()
     plt.show()
             
+def plot_tot(list_n_ini, funcs=[], funcs_names=[], t_ini=0, t_fin=100, nb_t=10000, displayed_sites=[], a=1):
+    # We plot the total number of bosons for multiple n_ini ; nb_sites is built-in in the len(n_ini[i]), which are assumed to all be the same
+    # kwarg funcs : optionally, other functions to be plotted for comparison
+    # kwarg funcs_names: name of the other functions, to put in labels
+    # kwargs t_ini, t_fin, t_nb : settings for t linspace
+    # kwarg a : a for rhs
+    if displayed_sites==[]:
+        displayed_sites = list(range(len(list_n_ini[0])))
+    t = np.linspace(t_ini,t_fin,nb_t)
+    plt.xscale('log')
+    plt.yscale('log')
+    for n_ini in list_n_ini:
+        n = odeint(rhs, n_ini, t, args=(a,))
+        for i in displayed_sites:
+            plt.plot(t, np.array([sum(x) for x in n]),label=f'$n_'+str(i)+'$'+r', $n_{ini}$='+str(n_ini))
+    for i in range(len(funcs)):
+            plt.plot(t, funcs[i](t),label=funcs_names[i])
+    plt.legend()
+    plt.show()
